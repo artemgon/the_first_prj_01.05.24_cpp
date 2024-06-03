bd2b3104f3736f344684130327e4ab5bdfed1013
artemgon <artemhoncharuk123@gmail.com>
2024-06-03 13:54:39 +0300

    Creating the first task of homework.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+	int quantity = 0;
+	for (int i = 100; i <= 900; i++)
+	{
+		if (i % 10 == i / 10 % 10 || i % 10 == i / 100 || i / 10 % 10 == i / 100)
+		{
+			quantity++;
+		}
+		else {
+			continue;
+		}
+	}
+	cout << "The result is " << quantity;
 	return 0;
 }
