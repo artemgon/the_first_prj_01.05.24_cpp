44f19987fe6240f04ae2cd80225973e0ca8f32ba
artemgon <artemhoncharuk123@gmail.com>
2024-06-03 14:00:14 +0300

    Modifying the first task of homework.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 	int quantity = 0;
-	for (int i = 100; i <= 900; i++)
+	for (int i = 100; i <= 999; i++)
 	{
 		if (i % 10 == i / 10 % 10 || i % 10 == i / 100 || i / 10 % 10 == i / 100)
 		{
