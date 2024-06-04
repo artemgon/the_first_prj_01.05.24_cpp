c231cbf81ca680603a44cd69cfd0d01ee1ca2ab6
artemgon <artemhoncharuk123@gmail.com>
2024-06-04 21:18:36 +0300

    Creating the third task of homework.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
+	int number, result_number = 0, factor = 1;
+	cout << "Enter a number: ";
+	cin >> number;
+	for (; number; number /= 10) {
+		if (number % 10 != 3 && number % 10 != 6) {
+			result_number += number % 10 * factor;
+			factor *= 10;
+		}
+	} 
+	cout << result_number;
 	return 0;
 }
