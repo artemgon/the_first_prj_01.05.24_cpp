69b9d3de5ccda0127a454346241e7fa7c2b87bcc
artemgon <artemhoncharuk123@gmail.com>
2024-06-05 13:59:26 +0300

    Creating the fouth task of homework.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+	unsigned int A, B;
+	for (;;) {
+		cout << "\nPlease, enter the number: ";
+		cin >> A;
+		for (B = 1; B <= A; B++)
+		{
+			if (B * B % A == 0 && B * B * B % A != 0)
+			{
+				cout << B << " ";
+			}
+		}
+	}
 	return 0;
 }
