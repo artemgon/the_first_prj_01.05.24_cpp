492980b7371196c28b9b9b45b66019bf8e1751b2
artemgon <artemhoncharuk123@gmail.com>
2024-06-06 12:24:43 +0300

    Creating the fifth task of homework.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+	int A, sum = 0; 
+	cout << "Please, enter a number: ";
+	cin >> A;
+	int B = A;
+	for (sum = 0; B > 0; sum += B % 10, B /= 10);
+	if (sum * sum * sum == A * A)
+	{
+		cout << "Yes";
+	}
+	else 
+	{
+		cout << "No";
+	}
 	return 0;
 }
