5cd35d952b2c4d4269f58df9f03a47d54e7f2d26
artemgon <artemhoncharuk123@gmail.com>
2024-06-06 11:51:46 +0300

    Creating the seventh task of homework.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+	int A, B;
+	cout << "Please, enter the numbers: ";
+	cout << "A: ";
+	cin >> A;
+	cout << "B: ";
+	cin >> B;
+	for (int i = 2;; i++)
+	{
+		if (A > B)
+		{
+			if (i <= B/2 && B % i == 0 && A % i == 0) cout << i << " ";
+			if (i > B / 2) break;
+		}
+		else if (B > A)
+		{
+			if (i <= A / 2 && B % i == 0 && A % i == 0) cout << i << " ";
+			if (i > A / 2) break;
+		}
+	}
 	return 0;
 }
